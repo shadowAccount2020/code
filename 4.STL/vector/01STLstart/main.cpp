989328509828acf602c5afc005a3b650f8365a74
mengchao <2205101365@qq.com>
2019-09-18 16:24:10 +0800

    添加文件

--- 4.STL/vector/01STLstart/main.cpp
+++ 4.STL/vector/01STLstart/main.cpp
@@ -18,18 +18,36 @@ public:
 int main()
 {
     vector<int> vi = {8,4,6,7,2,9,4,6,2,1,0};
+    //    sort(vi.begin(),vi.end(),Compare());
     //打印方式一：
-//    vector<int>::iterator itr;
-//    for(itr = vi.begin();itr!=vi.end();++itr)
-//    {
-//        cout << *itr << endl;
-//    }
+    //    vector<int>::iterator itr;
+    //    for(itr = vi.begin();itr!=vi.end();++itr)
+    //    {
+    //        cout << *itr << endl;
+    //    }
 
     //打印方式二：
-    sort(vi.begin(),vi.end(),Compare());
-        for(auto itr = vi.begin();itr!=vi.end();++itr)
-        {
-            cout << *itr << endl;
-        }
+
+    //    vi.insert(vi.end(),10);
+    vi.push_back(10);
+    vi.pop_back();
+    //    vi.clear();
+
+    for(auto itr = vi.begin();itr!=vi.end();++itr)
+    {
+        cout << *itr << endl;
+    }
+
+
+    int i = vi.back();
+    cout << i << endl;
+
+    cout << vi.size() << endl; //11
+    cout << vi.capacity() << endl;//22
+
+    vi.push_back(10);
+    cout << vi.size() << endl;
+    cout << vi.capacity() << endl;
+
     return 0;
 }
