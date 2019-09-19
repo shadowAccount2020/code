59bb91f49758419f13fda3f908773ae4b172e73b
mengchao <2205101365@qq.com>
2019-09-19 20:55:58 +0800

    添加文件

--- 4.STL/02list/list/main.cpp
+++ 4.STL/02list/list/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include<list>
+
 using namespace std;
 
 int main()
 {
-    list<int> li;
-    cout << "Hello World!" << endl;
+    list<int> li = {1,4,6,3,8,4,9,5};
+
+
+    li.emplace(li.begin());
+
+    cout << li.front() << endl;
+    cout << li.back() << endl;
+
+    for(auto itr = li.begin();itr != li.end();++itr)
+    {
+        cout << *itr << endl;
+    }
     return 0;
 }
