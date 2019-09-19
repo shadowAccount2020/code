7051acf5250774416a1c849d7163baff4251b463
孟超 <2205101365@qq.com>
2019-09-19 21:24:21 +0800

    Update main.c

--- 6.Linux API/fileIO/file/main.c
+++ 6.Linux API/fileIO/file/main.c
@@ -1,7 +1,28 @@
+/*************************************************************************                                                               
+    > File Name: file.c
+    > Author: 孟超
+    > Mail: 2205101365@qq.com 
+    > Created Time: 2019年09月19日 星期四 21时11分58秒
+ ************************************************************************/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
-//待补充
+
 int main()
 {
-    printf("Hello World!\n");
+    int fd = open("./a.txt",O_RDONLY);
+    if(fd<0)
+        _exit(-1);
+    printf("%d\n",fd);  
+        
+    char buf[100] = {0};
+    read(fd,&buf,100);
+    printf("%s\n",buf);
+
+
+    close(fd);
     return 0;
 }
