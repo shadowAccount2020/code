a74c0e0d9123022514701fc1982f74737e95675c
孟超 <2205101365@qq.com>
2019-09-19 21:52:55 +0800

    Update main.c

--- 6.Linux API/timerand/timerand/main.c
+++ 6.Linux API/timerand/timerand/main.c
@@ -1,7 +1,23 @@
+#include <time.h>
 #include <stdio.h>
-//待补充
+
+int main1()
+{
+    char *ch;
+    time_t t;
+    t = time(NULL);
+    ch = ctime(&t);//使用ctime也要使用上边的time
+    //printf("%d\n",t);
+    printf("%s\n",ch);
+    return 0;
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    struct tm *tt;
+    time_t t;
+    t = time(NULL);
+    tt = localtime(&t);                                                                                                                  
+    printf("%d\n",tt->tm_year);
     return 0;
 }
